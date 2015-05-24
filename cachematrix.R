755ae827b067a30291880e7ae8c9f20c462d1cdd
ccrittrs <sutekhtd@yahoo.com>
2015-05-24 15:53:20 -0400

    updated two methods: makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,54 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(orMat = matrix()) {
+  # create the empty matrix to hold the inverse
+  invMat <- matix()
+  
+  # set the initial matrix
+  set <- function(inMat){
+    orMat <- inMat
+    invMat <- matix()
+  }
+  
+  # return originl matrix
+  get <- function() orMat
+  
+  # create the inverted matrix
+  setInv <- function(solve){
+    invMat <<- solve
+  } 
+  
+  # return the inverted matrix (or empty matrix 
+  # if inverted matrix is not present)
+  getInv <- function() invMat
+  list(set = set, get = get, setInv = setInv, getInv = getInv)
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(orMat, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invMat <- orMat$getInv()
+  
+  # if invMat is not empty, we need to retrieve cached
+  if (!is.empty(invMat)){
+    message("getting cached data")
+    return(invMat)
+  } # else - we have to calculate it 
+  
+  # create temporary matrix to hold original data
+  tempMat <- orMat$get()
+  invMat <- solve(tempMat, ...)
+  orMat$setInv(invMat)
+  invMat
 }
+
+
+
+
+
+
+
